e2844b0b3650461c37eebba68f7f88dd93cea5cf
Esteban Alonzo <pstban10@gmail.com>
2024-10-13 09:41:42 -0300

    Agregamos un frontend para iniciar el proyecto

--- clases/models.py
+++ clases/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class TestClass(models.Model):
+    full_name = models.CharField(max_length=150)
+    date = models.DateField()
+    phone = models.CharField(max_length=20)
+    hour = models.TimeField()
+
+    HORARIOS = {
+        'lunes': ['8:00', '9:00', '15:00', '18:00', '19:15'],
+        'martes': ['18:00', '19:15'],
+        'miércoles': ['8:00', '9:00', '15:00', '18:00', '19:15'],
+        'jueves': ['18:00', '19:15'],
+        'viernes': ['8:00', '9:00', '15:00', '18:00', '19:15'],
+        'sábado': ['18:00', '19:15'],
+        'domingo': 'not classes buddy'
+    }
+
+    def __str__(self):
+        return f"{self.full_name} - {self.date}"
